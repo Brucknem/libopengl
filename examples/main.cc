7dfe5c6721dd84007b2ff803b61e4d5e7e5fb3a7
Brucknem <mbruckner94@gmail.com>
2020-07-13 13:10:30 +0200

    Removed CI

---
+++ examples/main.cc
@@ -0,0 +1,14 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <stdio.h>
+#include <iostream>
+#include "lib/windowing.h"
+#include <cassert>
+
+int main(int argc, char **argv)
+{
+    libopengl::BaseWindow *window = new libopengl::BaseWindow(4, 4, 800, 600, "LearnOpenGL");
+    window->MainLoop();
+
+    return 0;
+}
